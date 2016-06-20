01d52b549127d78c8635843a5331b6ecad30d3c7
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2016-06-20 17:18:38 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,8 @@
 
 var result = window.confirm('準備OK?');
 
-document.getElementById('choice').textContent = result;
+if(result){
+document.getElementById('choice').textContent = 'OKが押されました';
+}else{
+  document.getElementById('choice').textContent = 'キャンセルが押されました';
+}
