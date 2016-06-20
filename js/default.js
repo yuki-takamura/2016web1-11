1fb40bf0ffcb4771a613b571ccd29db516c77e97
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2016-06-20 17:24:24 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,8 +1,16 @@
 
-var result = window.confirm('準備OK?');
+var number = Math.floor(Math.random()*3);
+var answer = parseInt(window.prompt('数あてゲーム。0～2の数字を入力してください'));
 
-if(result){
-document.getElementById('choice').textContent = 'OKが押されました';
+ver message;
+if(answer === number){
+　message = 'あたり!'
+}else if(answer < number){
+  message = '残念!もっと大きいです';
+}else if(number < answer){
+  message = '残念!もっと小さいです';
 }else{
-  document.getElementById('choice').textContent = 'キャンセルが押されました';
+  message = '0～3の数字を入力してください';
 }
+
+document.getElementById('choice').textContent = message;
