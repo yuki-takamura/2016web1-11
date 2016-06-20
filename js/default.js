85a7e87f7260ac9fdbee37e922e561369e1476ac
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2016-06-20 17:32:20 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,13 +1,12 @@
 
 var number = Math.floor(Math.random()*3);
-var answer;
-var message;
 
-while(answer === number){
- answer = parseInt(window.prompt('数あてゲーム。0～2の数字を入力してください'));
-
+while(true){
+ var answer = parseInt(window.prompt('数あてゲーム。0～2の数字を入力してください'));
+ var message;
  if(answer === number){
 　 message = 'あたり!'
+　 break;
  }else if(answer < number){
    message = '残念!もっと大きいです';
  }else if(number < answer){
@@ -15,5 +14,6 @@ while(answer === number){
  }else{
    message = '0～3の数字を入力してください';
  }
-}
+} 
+ 
 document.getElementById('choice').textContent = message;
