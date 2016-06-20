049062e64e4d8791b854f1c74bcf0a0a291e61d6
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2016-06-20 17:26:02 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,7 +2,7 @@
 var number = Math.floor(Math.random()*3);
 var answer = parseInt(window.prompt('数あてゲーム。0～2の数字を入力してください'));
 
-ver message;
+var message;
 if(answer === number){
 　message = 'あたり!'
 }else if(answer < number){
